cf9ef41bb536c6190d3a31df1b5315a5cea7ec3f
PriyankaBamboriya26 <142428998+PriyankaBamboriya26@users.noreply.github.com>
2023-11-06 21:13:59 +0530

    Add files via upload

---
+++ To Do list/script.js
@@ -0,0 +1,41 @@
+const box=document.getElementById("box");
+const listContainer=document.getElementById("listcontainer");
+
+function addTask(){
+    if(box.value ===''){
+        alert("you must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = box.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    box.value = "";
+    saveData();
+
+
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
